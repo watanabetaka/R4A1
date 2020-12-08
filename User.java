21095a3db9a62ee92a35ca80bb46e8f8184edaa6
masuthan <74633279+masuthan@users.noreply.github.com>
2020-12-08 14:57:56 +0900

    Update User.java

--- User.java
+++ User.java
@@ -1,5 +1,28 @@
-packege bean;
-public class User implements
-while{
-sdlhsdflhjfsd
+package bean;
+
+public class User implements java.io.Serializable {
+
+	private String user_id;
+	 	private String  pass;
+
+	public String getuser_ID() {
+		return user_id;
+	}
+
+	public String getpass() {
+		return pass;
+	}
+
+
+
+
+	public void setuser_ID(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public void setpass(String pass) {
+		this.pass = pass;
+	}
+
+
 }
