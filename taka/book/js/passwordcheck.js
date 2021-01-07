50f64b867048f5bb55a6bca23d3daefc6612f204
watanabetaka <74642109+watanabetaka@users.noreply.github.com>
2021-01-08 08:43:45 +0900

    a

--- taka/book/js/passwordcheck.js
+++ taka/book/js/passwordcheck.js
@@ -19,16 +19,12 @@ $(document).ready(function() {
         // 文字数が7より大きいければ+1
         if (password.length > 7) strength += 1
         // 英字の大文字と小文字を含んでいれば+1
-        if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) strength += 1
+        if (password.match(/([a-z].*[A-Z])||([A-Z].*[a-z])/)) strength += 1
         // 英字と数字を含んでいれば+1
-        if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) strength += 1
-        // 記号を含んでいれば+1
-        if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
-        // 記号を2つ含んでいれば+1
-        if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+        if (password.match(/([0-9])/)) strength += 1
 
         // 点数を元に強さを計測
-        if (strength < 3) {
+        if (strength > 2) {
             $('#result').removeClass();
             document.getElementById("result").innerHTML = "適切です";
             if(document.getElementById("result").innerHTML=="適切です"&&document.getElementById("result_confirm").innerHTML=="適切です"){
@@ -41,6 +37,4 @@ $(document).ready(function() {
         }
 
       }
-
-
 });
